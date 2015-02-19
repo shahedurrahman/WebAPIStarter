251bf7f78059986a8ae6e6da66711bffe83cbce4
shahedurrahman <shahed.munna@gmail.com>
2015-02-20 01:57:23 +0600

    Added few api actions.

--- WebAPIStarter/Controllers/UserController.cs
+++ WebAPIStarter/Controllers/UserController.cs
@@ -4,6 +4,18 @@ namespace WebAPIStarter.Controllers
 {
     public class UserController : ApiController
     {
+        [HttpGet]
+        [Route("api/{controller}/{action}")]
+        public IHttpActionResult Get()
+        {
+            return Ok();
+        }
 
+        [HttpGet]
+        [Route("api/{controller}/{action}")]
+        public IHttpActionResult GetAll()
+        {
+            return Ok();
+        }
     }
 }
